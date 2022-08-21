69ad6924b7bec72c69278e19dbdf26f18044b4a9
rgoldsberry <107135474+rgoldsberry@users.noreply.github.com>
2022-08-21 10:51:32 -0600

    finished js funtions to build and filter table

--- static/js/app.js
+++ static/js/app.js
@@ -21,3 +21,27 @@ function buildTable(data) {
     );
   });
 }
+
+function handleClick() {
+  // get the date out of the box
+  let date = d3.select("#datetime").property("value");
+
+  // start with the raw data, then apply filters to it
+  let filteredData = tableData;
+
+  // if a date has been filtered, filter to that date. 
+  // otherwise, return full table
+  if (date) {
+    filteredData = filteredData.filter(row => row.datetime === date)
+  };
+
+  // rebuild the table with filtered data
+  buildTable(filteredData);
+
+}
+
+// listen for a click, then run handleClick function
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// load the full table initially
+buildTable(tableData);
