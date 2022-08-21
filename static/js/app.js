cca9d4472513be19aef647531755cf27b993cf8a
rgoldsberry <107135474+rgoldsberry@users.noreply.github.com>
2022-08-21 10:31:26 -0600

    began function to add table data

--- static/js/app.js
+++ static/js/app.js
@@ -3,3 +3,21 @@ const tableData = data;
 
 // reference the HTML table using d3
 var tbody = d3.select("tbody")
+
+function buildTable(data) {
+  // clear previous table
+  tbody.html("");
+
+  data.forEach((dataRow) => {
+    
+    // starts a new row for each item in array
+    let row = tbody.append("tr");
+    
+    // adds "table data" class for each value in that member of the array
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
+      }
+    );
+  });
+}
